be7da9f2a4a008f88bc4b8b375f7f421a6244262
Ritesh Dubey <0xchikku@gmail.com>
2023-06-04 17:21:32 +0530

    src file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
+//libraries
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+//pages
+import { ErrorPage, RootLayout, SavedJobs } from './pages';
+
+//components
+
+//css
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { MainContent } from './components';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <MainContent />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: 'savedjobs',
+        element: <SavedJobs />,
+        errorElement: <ErrorPage />,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
